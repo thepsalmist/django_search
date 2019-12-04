b2abf7badea89f40de4730cbdcf68c4f367b9112
thepsalmist <xf.xavierfrank@gmail.com>
2019-12-04 19:53:07 +0300

    create a helper function

--- core/views.py
+++ core/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.db.models import Q
 from .models import Journal
 
+# helper function to remove the repetition
+
+
+def is_valid_queryparam(param):
+    return param != '' and param is not None
+
 
 def BootstrapFilterView(request):
     qs = Journal.objects.all()
@@ -9,11 +15,11 @@ def BootstrapFilterView(request):
     id_exact_query = request.GET.get('ID_exact')
     title_or_author_query = request.GET.get('title_or_author')
 
-    if title_contains_query != '' and title_contains_query is not None:
+    if is_valid_queryparam(title_contains_query):
         qs = qs.filter(title__icontains=title_contains_query)
-    elif id_exact_query != '' and id_exact_query is not None:
+    elif is_valid_queryparam(id_exact_query):
         qs = qs.filter(id=id_exact_query)
-    elif title_or_author_query != '' and title_or_author_query is not None:
+    elif is_valid_queryparam(title_or_author_query):
         qs = qs.filter(Q(title_icontains=title_or_author_query) | Q(
             author__name__icontains=title_or_author_query)).distinct()
 

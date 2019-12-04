ba0bde93177df2eec78d44a709f668dfff376a89
thepsalmist <xf.xavierfrank@gmail.com>
2019-12-04 17:23:33 +0300

    modify views

--- core/views.py
+++ core/views.py
@@ -7,11 +7,12 @@ def BootstrapFilterView(request):
     title_contains_query = request.GET.get('title_contains')
     title_exact_query = request.GET.get('title_exact')
     title_or_author_query = request.GET.get('title_or_author')
-    context = {
-        'queryset': qs
-    }
 
     if title_contains_query != '' and title_contains_query is not None:
         qs = qs.filter(title__icontains=title_contains_query)
+
+    context = {
+        'queryset': qs
+    }
 
     return render(request, 'core/bootstrap_form.html', context)

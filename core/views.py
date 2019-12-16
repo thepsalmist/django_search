3a612c567a81be6e0063191a55e1040167fbb951
thepsalmist <xf.xavierfrank@gmail.com>
2019-12-16 16:12:31 +0300

    modify search query parameters

--- core/views.py
+++ core/views.py
@@ -14,14 +14,30 @@ def BootstrapFilterView(request):
     title_contains_query = request.GET.get('title_contains')
     id_exact_query = request.GET.get('ID_exact')
     title_or_author_query = request.GET.get('title_or_author')
+    view_count_min = request.GET.get('view_count_min')
+    view_count_max = request.GET.get('view_count_max')
+    date_min = request.GET.get('date_min')
+    date_max = request.GET.get('date_max')
 
     if is_valid_queryparam(title_contains_query):
         qs = qs.filter(title__icontains=title_contains_query)
     elif is_valid_queryparam(id_exact_query):
         qs = qs.filter(id=id_exact_query)
     elif is_valid_queryparam(title_or_author_query):
-        qs = qs.filter(Q(title_icontains=title_or_author_query) | Q(
+        qs = qs.filter(Q(title__icontains=title_or_author_query) | Q(
             author__name__icontains=title_or_author_query)).distinct()
+
+    if is_valid_queryparam(view_count_min):
+        qs = qs.filter(views__gte=view_count_min)
+
+    if is_valid_queryparam(view_count_max):
+        qs = qs.filter(views__lt=view_count_max)
+
+    if is_valid_queryparam(date_min):
+        qs = qs.filter(publish_date__gte=date_min)
+
+    if is_valid_queryparam(date_max):
+        qs = qs.filter(publish_date__lt=date_max)
 
     context = {
         'queryset': qs
